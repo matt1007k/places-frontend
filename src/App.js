38474359923de711eab4b6651529be365e3ee548
matt1007k <matt1007k@gmail.com>
2017-12-11 19:27:31 -0500

    Configuramos las rutas con react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React, { Component } from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 import './App.css';
+
+import {
+    BrowserRouter as Router,
+    Link,
+    Route
+} from 'react-router-dom';
 
 class App extends Component {
     constructor(props){
         super(props);
-
         this.state = {
-          numero: 0
+            numero: 0
         };
     }
 
-  render() {
-    return (
-        <MuiThemeProvider>
-            <Home/>
-        </MuiThemeProvider>
-    );
-  }
+    render(){
+        return(
+            <MuiThemeProvider>
+                <Router>
+                    <div>
+                        <Link to="/login">Login</Link>
+                        <Link to="/" style={{marginRight:'2em'}}>Home</Link>
+                        <Route exact path="/" component={Home}></Route>
+                        <Route path="/login" component={Login}></Route>
+                    </div>
+                </Router>
+            </MuiThemeProvider>
+        );
+    }
 }
 
 export default App;
